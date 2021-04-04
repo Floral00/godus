5fef880926219efa44a9e286c902b132eecc517c
Florent LESPINASSE <florentlespinasse@yahoo.fr>
2021-04-04 19:11:07 +0200

    correction bug session

--- session.php
+++ session.php
@@ -1,5 +1,9 @@
 <?php
-session_start();
+//On test si la session existe déjà (on évite ainsi d'ouvrir une session déjà existante)
+if(session_id() == '' || !isset($_SESSION)) {
+    session_start();
+}
+
 if (!isset($_SESSION['pseudo']))
 {
     $Verif_Utilisateur = 0;
